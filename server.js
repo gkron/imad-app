09d281f6477afce772a465c7a556696553a2c5a1
Ganesh Kumar <gkron18@gmail.com>
2017-08-18 10:05:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/artical-one', function(req, res){
+    res.sendFile('Artical one requested servered here')
+})
+
+app.get('/artical-two', function(req, res){
+    res.sendFile('Artical two requested servered here')
+})
+
+app.get('/artical-three', function(req, res){
+    res.sendFile('Artical three requested servered here')
+})
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
